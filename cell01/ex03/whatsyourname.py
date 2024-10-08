df293d0618b09c9714b0e72bb38ab7a5d6586a76
pamornra <porpiengamornra2551@gmail.com>
2024-10-08 10:54:34 +0700

    whatsyourname.py

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,6 @@
+first_name=input("what's your first name? : ")
+last_name=input("what's your last name? : ")
+namef=first_name.strip()
+namel=last_name.strip()
+name=namef +' '+ namel
+print("well pleased to meet you," ,name) 
