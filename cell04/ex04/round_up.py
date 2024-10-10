d7687f3e655dcb532a88e75c811b1e3635732417
pamornra <porpiengamornra2551@gmail.com>
2024-10-10 08:50:41 +0700

    round_up.py

--- cell04/ex04/round_up.py
+++ cell04/ex04/round_up.py
@@ -1,8 +1,3 @@
-num = float(input("give me some number: ")) 
-
-a = int(num)
-if num.is_integer() :
-  print(a)
-else :
-  s=a+(n % 1 > 0)
-  print(s)
+import math
+num= float(input("Give me some number: "))
+print(math.ceil(num))
